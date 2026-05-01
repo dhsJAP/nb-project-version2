22dcb1841ed2cc9d20f26bf21e4aa8bb39a234cc
Đặng Hồng Quốc Bảo <danghongquocbao@gmail.com>
2026-05-01 08:35:09 +0700

    feat: add remote image patterns to next.config for improved image handling

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,15 @@ const nextConfig: NextConfig = {
   turbopack: {
     root: path.resolve(__dirname),
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "vdxtnvakjykviesusvbw.supabase.co",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
